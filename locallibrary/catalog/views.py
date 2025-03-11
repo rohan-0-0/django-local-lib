2a2be205b856116dc0c1bdf72e62ccc5cb272834
Rohan Awal <awalrohan8@gmail.com>
2025-03-11 16:31:42 +0545

    Created a home page for the site with some dynamic content

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """View function for home page of site"""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books(status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is applied by default
+    num_authors = Author.objects.count()
+    
+    num_genre_word = Genre.objects.filter(name__icontains="fiction")
+                
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genre_word': num_genre_word,
+    }
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
